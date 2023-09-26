c8103f3a0d6739732be1d80c82a2e472327bee08
elperez1725 <elliott.perezramos@wsu.edu>
2023-09-26 14:00:34 -0700

    Finshed task 1 and 2; created a new User model and a new branch called milestone 3

--- app/Model/models.py
+++ app/Model/models.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from app import db
-
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 postTags = db.Table('postTags',
@@ -35,3 +34,22 @@ class Tag(db.Model):
 
     def __repr__(self):
        return f"<Tag id={self.id} name={self.name}>"
+    
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    password_hash = db.Column(db.String(128))
+
+
+    def __repr__(self):
+        return f"<User id={self.id} username={self.username} email={self.email}>"
+    
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    
+    def get_password(self):
+        return check_password_hash(self.password_hash)
