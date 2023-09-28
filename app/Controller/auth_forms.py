8a163a549e5d796fee5193ba2cd1f1feaeab05f0
elperez750 <elperez750@gmail.com>
2023-09-28 11:39:36 -0700

    updated database

--- app/Controller/auth_forms.py
+++ app/Controller/auth_forms.py
@@ -1,3 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import  ValidationError, DataRequired, EqualTo, Length,Email
+
+
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Username",  validators=[DataRequired(message="Username is required")])
+    email = StringField("Email", validators=[DataRequired(message="Email is required"), Email(message="Invalid email format")])
+    password1 = PasswordField("Password", validators=[DataRequired(message="Password is required")])
+    password2 = PasswordField("Confirm Password", validators=[DataRequired(message="Please confirm your password"), EqualTo('password1', message='passwords do not match')])
+    register = SubmitField("Register")
--- app/Controller/auth_routes.py
+++ app/Controller/auth_routes.py
@@ -3,11 +3,26 @@ import sys
 from flask import Blueprint
 from flask import render_template, flash, redirect, url_for
 from config import Config
+from app.Controller.auth_forms import RegistrationForm
+from app.Model.models import User
 
 from app import db
+
+
 
 bp_auth = Blueprint('auth', __name__)
 bp_auth.template_folder = Config.TEMPLATE_FOLDER 
 
-# @bp_auth.route('/register', methods=['GET', 'POST'])
-# def register():
+@bp_auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        new_user = User(username=form.username.data, email=form.email.data)
+        new_user.set_password(form.password1.data)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in','success')
+        return redirect(url_for('routes.index'))
+    else:
+        print(form.errors)
+    return render_template('register.html', form=form)
